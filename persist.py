5dbc12fc1e5b66ead6a08618241dbc649a858b31
Sumedh Rasal <sumedh.rasal@here.com>
2023-08-19 09:17:24 -0500

    retrieve docs from weaviate

--- persist.py
+++ persist.py
@@ -12,8 +12,6 @@ class Persist:
         self.data_directory = 'data'
         self.embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
         self.client = weaviate.Client(url=WEAVIATE_URL)
-        # self.db = Chroma(persist_directory="./chroma_db", embedding_function=self.embedding_function)
-        pass
 
     def save_content_to_location(self, topic_name, file_contents):
         if not os.path.exists(self.data_directory + '/' + topic_name):
@@ -23,25 +21,24 @@ class Persist:
             with open(filepath, 'w') as f:
                 f.write(contents)
 
-    def load_content_to_db(self, topic_name):
+    def query_from_the_db(self, topic_name, query):
         try:
             topic_directory = self.data_directory + '/' + topic_name
             files = [_ for _ in os.listdir(topic_directory) if os.path.isfile(os.path.join(topic_directory, _))]
+            documents = []
             for f in files:
                 # load the document and split it into chunks
-                loader = TextLoader(f)
-                documents = loader.load()
+                loader = TextLoader(os.path.join(topic_directory, f))
+                documents.extend(loader.load())
 
-                # split it into chunks
-                text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-                docs = text_splitter.split_documents(documents)
+            # split it into chunks
+            text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+            docs = text_splitter.split_documents(documents)
 
-                # load it into Chroma
-                # self.db = Chroma.from_documents(docs, self.embedding_function, persist_directory="./chroma_db")
-                # load it into Weaviate
-                self.db = Weaviate.from_documents(docs, self.embedding_function, client=self.client, by_text=False)
+            # load it into Chroma
+            # db = Chroma.from_documents(docs, self.embedding_function, persist_directory="./chroma_db")
+            # load it into Weaviate
+            db = Weaviate.from_documents(docs, self.embedding_function, client=self.client, by_text=False)
+            return db.similarity_search_with_score(query)
         except Exception as e:
             print('Exception occurred to load content in the DB', e)
-
-    def query_from_the_db(self, query):
-        return self.db.similarity_search(query)
